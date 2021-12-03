41b5beb7ff2d958755278384c4e2f297821751d6
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2021-12-03 14:12:31 +0100

    Update assert.m
    Add local function for checking if toolboxes are on path

--- code/+nansen/assert.m
+++ code/+nansen/assert.m
@@ -4,13 +4,21 @@ function assert(keyword)
 %   nansen.assert(KEYWORD) throws an error if the assertion for the given
 %   keyword is not valid.
 
+% Todo: make assertIsInstalled function? Improve that before using the
+% code
+
     switch keyword
         
-        case 'WidgetsToolboxInstalled'
-            
-            msg = ['The Widgets Toolbox is required, but is not ', ...
-                   'found on MATLAB''s searchpath.'];
+        case 'ExtractOnSavepath'
+            errID = 'Nansen:ExtractNotFoundOnPath';
+            msg = ['EXTRACT is required for this operation, but is not ', ...
+                   'found on MATLAB''s search path.'];
+            assertionValid = exist('run_extract', 'file') == 2;
 
+        case 'WidgetsToolboxInstalled'
+            errID = 'Nansen:WidgetsToolboxNotInstalled';
+            msg = ['The Widgets Toolbox is required, but is not ', ...
+                   'found on MATLAB''s search path.'];
             assertionValid = exist('widgetsRoot', 'file') == 2;
                 
         otherwise
@@ -19,8 +27,47 @@ function assert(keyword)
     end
 
     if ~assertionValid
-        exception = MException('Nansen:WidgetsToolboxNotInstalled', msg);
+        exception = MException(errID, msg);
         throwAsCaller(exception)
     end
 
 end
+
+
+
+
+% Local functions...
+function [tf, exception] = assertToolboxInstalled(keyword)
+    
+    exception = MException.empty;
+    
+    S = struct();
+    
+    switch keyword
+        case 'ExtractOnSavepath'
+            S.ToolboxName = 'EXTRACT';
+            S.FunctionName = 'run_extract';
+            
+        case 'WidgetsToolboxInstalled'
+        	S.ToolboxName = 'The Widgets Toolbox';
+            S.FunctionName = 'widgetsRoot';
+            
+        case 'Caiman'
+            S.ToolboxName = 'CaImAn';
+            S.FunctionName = 'deconvolveCa';
+            
+    end
+
+    tf = exist(S.FunctionName, 'file') == 2;
+    
+    if ~tf
+        errID = sprintf('Nansen:%sNotInstalled', S.ToolboxName);
+        msg = ['%s is required for this operation, but is not ', ...
+                   'found on MATLAB''s search path.', S.ToolboxName];
+        
+        exception = MException(errID, msg);
+    end
+
+end
+
+
