978e1ba17c8db23ff6ac9d75e29dfe8bba536be4
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-02-28 12:15:38 +0100

    Update datalocation table variable definition

--- code/+nansen/+metadata/+tablevar/DataLocation.m
+++ code/+nansen/+metadata/+tablevar/DataLocation.m
@@ -1,5 +1,7 @@
 classdef DataLocation < nansen.metadata.abstract.TableVariable
-    
+%DataLocation Controls behavior of table cells containing datalocation items.
+%
+%   
     
     properties (Constant)
         IS_EDITABLE = false
@@ -19,19 +21,107 @@ classdef DataLocation < nansen.metadata.abstract.TableVariable
    
     methods
         function obj = DataLocation(S)
+            if nargin < 1; S = struct.empty; end
             obj@nansen.metadata.abstract.TableVariable(S);
-            assert(isstruct(obj.Value), 'Value must be a struct')
+            assert( all( arrayfun(@isstruct, [obj.Value])), 'Value must be a struct')
         end
     end
    
    
     methods
+        
         function str = getCellDisplayString(obj)
-            numDataLocations = numel(fieldnames(obj.Value));
-            str = sprintf('%d Datalocations', numDataLocations);
+        %getCellDisplayString Get character vector to display in table cell
+            
+        % todo: vectorize
+            
+            % Assume that the current datalocation model is valid. This
+            % is a weak assumption, since it is in theory possible to have
+            % a call this function with values from sessions/metatables
+            % that are not part of the current project...
+            dataLocationModel = nansen.config.dloc.DataLocationModel;
+            
+            str = cell(numel(obj), 1);
+            
+            for i = 1:numel(obj)
+                
+                if ~isfield(obj(i).Value, 'Uuid')
+                    dataLocationNames = fieldnames(obj(i).Value);
+                else % legacy code (when each dataloc is a field...):
+                    if ~isfield(obj(i).Value, 'Name')
+                        obj(i).Value = dataLocationModel.expandDataLocationInfo(obj(i).Value);
+                    end
+                    dataLocationNames = {obj(i).Value.Name};
+                end
+                
+                numDataLocations = numel(dataLocationNames);
+                thisStr = '<html>';
+
+                for j = 1:numDataLocations
+                    
+                    if isfield(obj(i).Value, 'RootPath')
+                        rootPath = obj(i).Value(j).RootPath;
+                        subFolder = obj(i).Value(j).Subfolders;
+                    else % legacy code (when each dataloc is a field...):
+                        rootPath = obj(i).Value.(dataLocationNames{j});
+                        subFolder = rootPath;
+                    end
+                    
+                    if isempty(rootPath)
+                        tmpStr = obj.getIconHtmlString('dot_off');
+                    elseif isfolder(fullfile(rootPath, subFolder)) && ~isempty(subFolder)
+                        tmpStr = obj.getIconHtmlString('dot_on');
+                    elseif isfolder(subFolder) % ref legacy...
+                        tmpStr = obj.getIconHtmlString('dot_on');
+                    elseif isempty(subFolder)
+                        tmpStr = obj.getIconHtmlString('dot_off');
+                    else
+                        tmpStr = obj.getIconHtmlString('dot_none');
+                    end
+
+                    thisStr = strcat(thisStr, tmpStr);
+
+                end
+
+                if numDataLocations == 0
+                    thisStr = sprintf('%d Datalocations', numDataLocations);
+                end
+                
+                str{i} = thisStr;
+            end
+            
         end
        
         function str = getCellTooltipString(obj)
+        %getCellTooltipString Get character vector to display as tooltip
+        
+            datalocStruct = obj.Value;
+            
+            if isa(datalocStruct, 'cell')
+                datalocStruct = datalocStruct{1};
+            end
+            
+            if isempty(datalocStruct)
+                str = '';
+            else
+                
+                % Create a html formatted string from values in struct
+                str = cell(size(datalocStruct));
+                strtab = '&nbsp;&nbsp;&nbsp;&nbsp;';
+                for i = 1:numel(datalocStruct)
+                    str{i} = sprintf('%s (%s)<br/>%s Root: %s<br />%s Folder: %s', ...
+                        datalocStruct(i).Name, char( datalocStruct(i).Type ), ...
+                        strtab, datalocStruct(i).RootPath, ...
+                        strtab, datalocStruct(i).Subfolders);
+                end
+                
+                str = strjoin(str, '<br /><br />'); % Add blank line between data locations
+                str = sprintf('<html><div align="left"> %s </div>', str);
+                                
+            end
+        end
+        
+        function str = getCellTooltipStringOld(obj)
             
             datalocStruct = obj.Value;
             
@@ -43,7 +133,7 @@ classdef DataLocation < nansen.metadata.abstract.TableVariable
                 str = '';
             else
                 % Format struct into a multiline string:
-                structStr = evalc('disp(datalocStruct)');
+                %structStr = evalc('disp(datalocStruct)');
                 
                 while true % Remove trailing newlines...
                     if strcmp(structStr(end),  sprintf('\n'))                   %#ok<SPRINTFN>
@@ -64,25 +154,113 @@ classdef DataLocation < nansen.metadata.abstract.TableVariable
             end
         end
        
-        function value = update(obj, sessionObj)
-                  
-            value = obj.Value;
+        function onCellDoubleClick(obj, metaObj)
             
-            global dataLocationModel % Todo: dont use global
-            if isempty(dataLocationModel)
-                dataLocationModel = nansen.setup.model.DataLocations();
+            if ~isempty(obj.Value)
+                
+                S = struct();
+                for i = 1:metaObj.DataLocationModel.NumDataLocations
+                    thisDataLoc = metaObj.DataLocationModel.Data(i);
+                    
+                    fieldName = thisDataLoc.Name;
+                    fieldName_ = strcat(fieldName, '_');
+                    
+                    rootPath = metaObj.getDataLocationRootDir(fieldName);
+                    allRootPaths = {thisDataLoc.RootPath.Value};
+                    
+                    if isempty(rootPath)
+                        rootPath = '';
+                    end
+                    
+                    S.(fieldName) = rootPath;
+                    if ~any(strcmp(rootPath, allRootPaths))
+                        allRootPaths = [{rootPath}, allRootPaths];
+                    end
+                    S.(fieldName_) = allRootPaths;
+                    
+                end
+                
+                h = structeditor.App(S, 'AdjustFigureSize', true, ...
+                    'Title', 'Edit Data Location Rootpaths', ...
+                    'LabelPosition', 'over', ...
+                    'Prompt', 'Select datalocation root directories');
+                h.Title = sprintf('Edit Data Locations for %s', metaObj.sessionID);
+
+                h.waitfor()
+                
+                if h.wasCanceled
+                    return
+                else
+                    sNew = h.dataEdit;
+                    if ~isequal(sNew, S)
+                        metaObj.updateRootDir(sNew)
+                    end
+                end
+                
             end
+            
+        end
+        
+    end
+    
+    methods (Static)
+        
+        function value = updateDataLocation(sessionObj)
+                  
+            % Todo: Change name to update when finished. then datalocation
+            % will be updatable from sessionbrowser.
+            
+            % Todo: Make sure root uid is assigned correctly...
+            
+            error('not implemented')
+            value = sessionObj.DataLocation;
+            
+            dataLocationModel = sessionObj.DataLocationModel;
+            
+            % Todo: Make sure this works even if rootpath identity
+            % changed..
             
             for i = 1:numel(dataLocationModel.Data)
                 thisLoc = dataLocationModel.Data(i);
                 pathString = sessionObj.detectSessionFolder(thisLoc);
                 if ~isempty(pathString)
-                    value.(thisLoc) = pathString; 
+
+                    folders = strsplit(pathString, filesep);
+                    numFolders = numel(thisLoc.SubfolderStructure);
+                    
+                    value(i).RootPath = strjoin(folders(1:end-numFolders), filesep);
+                    value(i).Subfolders = strjoin(folders(end-numFolders+1:end), filesep);
+                    
                 end
             end
             
         end
-   end
-    
+    end
+   
+    methods (Static)
+        
+        function str = getIconHtmlString(iconName, iconSize)
+        %getIconHtmlString Return html string for icon with given name
+        %
+        %   str = <CLASSNAME>.getIconHtmlString(iconName)
+        %
+        %   str = <CLASSNAME>.getIconHtmlString(iconName, iconSize)
+        %
+        %   Input:
+        %       iconName : 'dot_on' | 'dot_off'
+        
+            if nargin < 2
+                iconSize = 16;
+            end
+        
+            iconPath = fullfile(nansen.rootpath, '+metadata', '+tablevar', '_symbols');
+            iconPath = fullfile(iconPath, sprintf('%s.png', iconName));
+            
+            sizeSpec = sprintf('width="%d" height="%d"', iconSize, iconSize);
+            str = sprintf('<img src="file:%s" %s margin="0">', iconPath, sizeSpec);
+            
+        end
+        
+    end
     
 end
