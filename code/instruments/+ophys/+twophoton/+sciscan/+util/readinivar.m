554745ddd4b7c8ea98ea9c28e66802e434e5ec54
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-02-28 11:34:07 +0100

    add instrument module for sciscan

---
+++ code/instruments/+ophys/+twophoton/+sciscan/+util/readinivar.m
@@ -0,0 +1,29 @@
+function varvalue = readinivar(inistring,variablename)
+
+ind1=regexp([inistring ' '],variablename);
+ind2=regexp(inistring,'\n');
+ind2(end+1) = numel(inistring);
+
+varvalue=[];
+
+if ~isempty(ind1)
+    
+    varline=inistring(ind1(1):(ind2(ind2>ind1(1))));
+    
+    s2=regexp(varline,'\=|\"','split');
+    
+    for i=2:length(s2)
+        if sum(size(strtrim(s2{i})))
+            varvalue = regexprep(s2{i}, ',', '.');
+            varvalue = str2num(varvalue);
+            if ~isempty(varvalue)
+                break
+            else
+                varvalue=s2{i};
+                break
+            end
+        end
+    end
+end
+  
+end
