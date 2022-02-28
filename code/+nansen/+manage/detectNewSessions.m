7482182ca01acf7c873d1e9c9341d881c033c834
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-02-28 12:52:03 +0100

    Update detectNewSessions.m
    Updated based on updated session schema

--- code/+nansen/+manage/detectNewSessions.m
+++ code/+nansen/+manage/detectNewSessions.m
@@ -1,43 +1,53 @@
-function newSessionObjects = detectNewSessions(metaTable, dataLocationType)
-
-
+function newSessionObjects = detectNewSessions(metaTable, dataLocationName)
+%detectNewSessions Detect new sessions associated with a metatable
+%
+%   newSessionObjects = detectNewSessions(metaTable, dataLocationName)
+%   look for session folders based on the current datalocation model 
+%   (i.e current project) and make a list of session objects based on
+%   folders. Session objects for all sessions that are not present in the
+%   table is returned.
+%   
+%   INPUTS:
+%       metaTable : a session metatable
+%       dataLocationName : (Optional) Name of datalocation. Default is 'all'
+%
+    
     import nansen.dataio.session.listSessionFolders
     import nansen.dataio.session.matchSessionFolders
     
-    global dataLocationModel
-
-    filePath = nansen.localpath('DataLocationSettings');
-    dataLocationModel = nansen.setup.model.DataLocations(filePath);
-
+    % Get current data location model. Todo: What if there are situations
+    % where another datalocation model should be used?
+    filePath = nansen.localpath('DataLocationSettings');    
+    dataLocationModel = nansen.config.dloc.DataLocationModel(filePath);
     
     
-% %     if nargin < 2 || isempty(dataLocationType)
-% %         dataLocationType = dlModel.Data(1).Name;
-% %     end
-% %     
-% %     msg = sprintf('Data location type (%s) is not configured', dataLocationType);
-% %     assert(contains(dataLocationType, {dlModel.Data.Name}), msg)
-
-    %sessionSchema = @nansen.metadata.schema.vlab.TwoPhotonSession;
-            
+    if nargin < 2 || isempty(dataLocationName)
+        %dataLocationName = dataLocationModel.DefaultDataLocation;
+        dataLocationName = 'all';
+    end
+    
+    if ~strcmp(dataLocationName, 'all')
+        msg = sprintf('Data location (%s) does not exist', dataLocationName);
+        assert(any(dataLocationModel.containsItem(dataLocationName)), msg)
+    end
+    
     % % Use the folder structure to detect session folders.
     sessionFolders = listSessionFolders(dataLocationModel, 'all');
     sessionFolders = matchSessionFolders(dataLocationModel, sessionFolders);
-
     
     if isempty(sessionFolders)
         return
     end
 
     % Todo: Get schema based on selection
-    sessionSchema = @nansen.metadata.schema.vlab.TwoPhotonSession;
-
+    sessionSchema = @nansen.metadata.type.Session;
+    args = {'DataLocationModel', dataLocationModel};
     
     % Create a list of session metadata objects
     numSessions = numel(sessionFolders);
     sessionArray = cell(numSessions, 1);
     for i = 1:numSessions
-        sessionArray{i} = sessionSchema(sessionFolders(i));
+        sessionArray{i} = sessionSchema(sessionFolders(i), args{:});
     end
 
     sessionArray = cat(1, sessionArray{:});
