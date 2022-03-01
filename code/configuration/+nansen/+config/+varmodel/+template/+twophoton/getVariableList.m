09ae331493ab7748bc60b2fb61324a8466381b11
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-03-01 13:41:00 +0100

    update list of two photon data variables

--- code/configuration/+nansen/+config/+varmodel/+template/+twophoton/getVariableList.m
+++ code/configuration/+nansen/+config/+varmodel/+template/+twophoton/getVariableList.m
@@ -2,30 +2,84 @@ function variableList = getVariableList()
 
     % Initialize struct array with default fields.
     variableList = nansen.config.varmodel.VariableModel.getBlankItem();
-
+    
+    i = 1;
     % Original (raw) two photon recording
-    variableList(1).VariableName = 'TwoPhotonSeries_Original';
-    variableList(1).IsDefaultVariable = true;
+    variableList(i).VariableName = 'TwoPhotonSeries_Original';
+    variableList(i).IsDefaultVariable = true;
 
+    i = i+1;
     % Corrected (motion-corrected) two photon recording
-    variableList(2).VariableName = 'TwoPhotonSeries_Corrected';
-    variableList(2).IsDefaultVariable = true;
-    variableList(2).FileNameExpression = 'two_photon_corrected';
-    variableList(2).DataLocation = 'Processed';
-    variableList(2).FileType = '.raw';
-    variableList(2).Subfolder = 'image_registration';
+    variableList(i).VariableName = 'TwoPhotonSeries_Corrected';
+    variableList(i).IsDefaultVariable = true;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'image_registration';
+    variableList(i).FileNameExpression = 'two_photon_corrected';
+    variableList(i).FileType = '.raw';
 
+    i = i+1;
     % Roi masks for corrected two photon series
-    variableList(3).VariableName = 'RoiMasks';
-    variableList(3).IsDefaultVariable = false;
-    variableList(3).FileNameExpression = 'roi_masks';
-    variableList(3).DataLocation = 'Processed';
-    variableList(3).FileType = '.mat';
-
-%             variableList(4).VariableName = 'RoiResponses_Original';
-%             variableList(5).VariableName = 'RoiResponses_DfOverF';
-
-
+    variableList(i).VariableName = 'RoiMasks';
+    variableList(i).IsDefaultVariable = false;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'roi_data';
+    variableList(i).FileNameExpression = 'roi_masks';
+    variableList(i).FileType = '.mat';
+    
+    i = i+1;
+    % Curated roi masks for corrected two photon series
+    variableList(i).VariableName = 'RoiMasksCurated';
+    variableList(i).IsDefaultVariable = false;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'roi_data';
+    variableList(i).FileNameExpression = 'roi_masks_curated';
+    variableList(i).FileType = '.mat';
+    
+    i = i+1;
+    % Array of roi objects
+    variableList(i).VariableName = 'RoiArray';
+    variableList(i).IsDefaultVariable = false;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'roi_data';
+    variableList(i).FileNameExpression = 'roi_array';
+    variableList(i).FileType = '.mat';
+    
+    i = i+1;
+    % Curated array of roi objects
+    variableList(i).VariableName = 'RoiArrayCurated';
+    variableList(i).IsDefaultVariable = false;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'roi_data';
+    variableList(i).FileNameExpression = 'roi_array_curated';
+    variableList(i).FileType = '.mat';
+    
+    i = i+1;
+    % RoiResponseSeries
+    variableList(i).VariableName = 'RoiResponseSeries_Original';
+    variableList(i).IsDefaultVariable = false;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'roi_data';
+    variableList(i).FileNameExpression = 'roiresponse_original';
+    variableList(i).FileType = '.mat';
+        
+    i = i+1;
+    % RoiResponseSeries Dff
+    variableList(i).VariableName = 'RoiResponseSeries_Dff';
+    variableList(i).IsDefaultVariable = false;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'roi_data';
+    variableList(i).FileNameExpression = 'roiresponse_dff';
+    variableList(i).FileType = '.mat';
+    
+    i = i+1;
+    % RoiResponseSeries Deconvolved
+    variableList(i).VariableName = 'RoiResponseSeries_Deconvolved';
+    variableList(i).IsDefaultVariable = false;
+    variableList(i).DataLocation = '';
+    variableList(i).Subfolder = 'roi_data';
+    variableList(i).FileNameExpression = 'roiresponse_deconvolved';
+    variableList(i).FileType = '.mat';
+    
 
 end
             
