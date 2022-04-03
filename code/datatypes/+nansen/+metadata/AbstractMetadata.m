be234411b20d8fb83e4e17c7b573b8da03afd036
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-04-03 14:40:46 +0200

    Update AbstractMetaData
    Added some methods and extend functionality so that yaml files can have a .txt extension.

--- code/datatypes/+nansen/+metadata/AbstractMetadata.m
+++ code/datatypes/+nansen/+metadata/AbstractMetadata.m
@@ -1,8 +1,21 @@
 classdef AbstractMetadata < dynamicprops
 %AbstractMetadata Abstract class for metadata
 
+% This class provides interface for loading and saving metadata from the
+% properties of implementing subclasses. Could be generalized to accept
+% different formats like json, xml, ini etc..
+
+% This is a bit of a mess because I wanted to have a preference for saving
+% files with txt extension (to support preview of files on mac without 
+% installing plugins)
+
     properties (Access = protected)
-        Filename
+        Filename        
+    end
+    
+    properties (Dependent, Access = private)
+        ExistAsYaml % Does metadata yaml file exist?
+        ExistAsText % Does metadata yaml file with .txt extension exist?
     end
     
     methods
@@ -13,25 +26,38 @@ classdef AbstractMetadata < dynamicprops
             [filepath, name, ~] = fileparts(filepath);
             filename = fullfile(filepath, [name, '.yaml']);
             
-            appendTxt = getpref('nansen', 'SaveYamlAsTxt', false);
-            if appendTxt
-                filename = strcat(filename, '.txt');
-            end
-            
             obj.Filename = filename;
-            
         end
         
         function readFromFile(obj)
             if isempty(obj.Filename); return; end
-            S = yaml.ReadYaml(obj.Filename);
-            obj.fromStruct(S)
+            
+            filepath = obj.getFilepathRead();
+            if ~isempty(filepath)
+                S = yaml.ReadYaml(filepath);
+                obj.fromStruct(S)
+            end
         end
 
         function writeToFile(obj)
             if isempty(obj.Filename); return; end
             S = obj.toStruct();
-            yaml.WriteYaml(obj.Filename, S);
+            filepath = obj.getFilepathWrite();
+            yaml.WriteYaml(filepath, S);
+            
+            obj.checkForDuplicateFiles()
+        end
+        
+        function deleteFile(obj)
+        
+            if obj.ExistAsYaml
+                delete(obj.Filename)
+            end
+            
+            if obj.ExistAsText        
+                delete( strcat(obj.Filename, '.txt') )
+            end
+            
         end
         
         function set(obj, name, value)
@@ -50,9 +76,12 @@ classdef AbstractMetadata < dynamicprops
             
         end
     end
-        
     
     methods (Access = protected)
+        
+        function tf = isfile(obj)
+            tf = obj.ExistAsYaml || obj.ExistAsText;
+        end
         
         function S = toStruct(obj)
             
@@ -90,9 +119,62 @@ classdef AbstractMetadata < dynamicprops
         end
        
         function propertyNames = getPropertyNames(obj)
+        %getPropertyNames Get list of property names
             propertyNames = properties(obj);
+            
+            % Subclasses can override. Usefeul for controlling which
+            % propertynames are written to file.
         end
         
     end
-
+    
+    methods (Access = private)
+        
+        function filepath = getFilepathRead(obj)
+            
+            if obj.ExistAsYaml
+                filepath = obj.Filename;
+            elseif obj.ExistAsText
+                filepath = [obj.Filename, '.txt'];
+            else
+                filepath = '';
+            end
+            
+        end
+        
+        function filepath = getFilepathWrite(obj)
+            appendTxt = getpref('nansen', 'SaveYamlAsTxt', false);
+            if appendTxt
+                filepath = strcat(obj.Filename, '.txt');
+            else
+                filepath = obj.Filename;
+            end
+        end
+        
+        function checkForDuplicateFiles(obj)
+        %checkForDuplicateFiles Delete .yaml or .txt file if both exist
+        
+            if obj.ExistAsYaml && obj.ExistAsText        
+                appendTxt = getpref('nansen', 'SaveYamlAsTxt', false);
+                if appendTxt
+                    delete(obj.Filename)
+                else
+                    delete( strcat(obj.Filename, '.txt') )
+                end
+            end
+        end
+    end
+    
+    methods
+        
+        function tf = get.ExistAsYaml(obj)
+            tf = exist(obj.Filename, 'file')==2;
+        end
+        
+        function tf = get.ExistAsText(obj)
+            tf = exist([obj.Filename,'.txt'], 'file')==2;
+        end
+        
+    end
+    
 end
