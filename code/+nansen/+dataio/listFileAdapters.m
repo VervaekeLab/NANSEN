dbf20a796b4dc8d5346e8aab1c4fbba8cd639888
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-04-04 10:20:40 +0200

    Update listFileAdapters.m

--- code/+nansen/+dataio/listFileAdapters.m
+++ code/+nansen/+dataio/listFileAdapters.m
@@ -1,11 +1,24 @@
 function fileAdapterList = listFileAdapters()
 %listFileAdapters Create a list of file adapters
 %
-%   nansen.dataio.listFileAdapters
-    se
+%   fileAdapterList = nansen.dataio.listFileAdapters() returns a struct
+%   array containing information about file adapters.
+%
+%   The fileAdapterList struct array contains the following fields:
+%       FileAdapterName     (char) : Name of fileadapter 
+%       FunctionName        (char) : Name of function for file adapter
+%       SupportedFileTypes  (cell) : File types that are supported with this fileadapter
+%       DataType            (char) : Name of datatype returned by this file adapter
     
+    
+    % Todo: Ignore file adapters with a name that are already in the list
+    % Todo: Start adding from project dir, then watchfolder, then internal?
+    
+    rootPath = {};
+
     % Get folder containing file adapters from nansen core package, ...
-    
+    rootPath{end+1} = fullfile(nansen.rootpath, '+dataio', '+fileadapter');
+    rootPath{end+1} = fullfile(nansen.localpath('integrations'), 'fileadapters');
     
     % ... from project folder
     
@@ -13,6 +26,36 @@ function fileAdapterList = listFileAdapters()
     % ... and from watch folders.
     
     
+    % Find all subfolders and list m files.
+    fileAdapterFolders = utility.path.listSubDir(rootPath, '', {}, inf);
+    fileAdapterMfiles = utility.path.listFiles(fileAdapterFolders, '.m');
     
+    fileAdapterList = struct('FileAdapterName', 'Default', 'FunctionName', 'load', ...
+        'SupportedFileTypes', {{'mat'}}, 'DataType', 'N/A');
+    
+    count = 2;
+    
+    % Loop through m-files and add to file adapter list if this 
+    for i = 1:numel(fileAdapterMfiles)
+        
+        thisFcnName = utility.path.abspath2funcname(fileAdapterMfiles{i});
+        mc = meta.class.fromName(thisFcnName);
+        
+        if ~isempty(mc) && isa(mc, 'meta.class') && ...
+                contains('nansen.dataio.FileAdapter', {mc.SuperclassList.Name} )
+        
+            [~, fileName] = fileparts(fileAdapterMfiles{i});
+        
+            fileAdapterList(count).FileAdapterName = fileName;
+            fileAdapterList(count).FunctionName = thisFcnName;
+            isProp = strcmp({mc.PropertyList.Name}, 'SUPPORTED_FILE_TYPES');
+            fileAdapterList(count).SupportedFileTypes = mc.PropertyList(isProp).DefaultValue;
+            isProp = strcmp({mc.PropertyList.Name}, 'DataType');
+            fileAdapterList(count).DataType = mc.PropertyList(isProp).DefaultValue;
+            count = count + 1;
+            
+        end
+    end
+
 
 end
