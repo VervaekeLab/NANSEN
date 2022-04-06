7c32882a895ec07c3a7059badaf36d6078bfe7c6
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-04-06 10:59:37 +0200

    Update HasDialogBox.m
    Added methods for activating/deactivating global state + fixed bug

--- code/graphics/+applify/+mixin/HasDialogBox.m
+++ code/graphics/+applify/+mixin/HasDialogBox.m
@@ -4,15 +4,41 @@ classdef HasDialogBox < uim.handle
         DialogBox
     end
     
-    methods 
+    methods
         
-        function displayMessage(obj, message, target, msgDuration)
+        function C = activateGlobalMessageDisplay(obj, mode)
+
+            if nargin < 2
+                mode = 'update';
+            end
+
+            global fprintf
+
+            switch mode
+                case 'display'
+                    fprintf = @(msg)obj.DialogBox.displayMessage(msg);
+                case 'update'
+                    fprintf = @(varargin)obj.DialogBox.displayMessage(varargin{:});
+            end
+
+            C = onCleanup(@obj.deactivateGlobalMessageDisplay);
+
+        end
+        
+        function deactivateGlobalMessageDisplay(obj)
+            global fprintf
+            fprintf = str2func('fprintf');
+            obj.DialogBox.clearMessage()
+        end
+        
+        
+        function displayMessage(obj, message, msgDuration)
             
             if isempty(obj.DialogBox)
                 fprintf([message, '\n']); return
             end
             
-            if nargin < 4; msgDuration = []; end
+            if nargin < 3; msgDuration = []; end
 
             obj.DialogBox.displayMessage(message, msgDuration)
 
@@ -21,7 +47,7 @@ classdef HasDialogBox < uim.handle
         function clearMessage(obj)
             
             if isempty(obj.DialogBox)
-                fprintf([message, '\n']); return
+                return
             end
             
             if isvalid(obj) && ~isempty(obj.DialogBox)
