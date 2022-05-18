a19e2693ea0c8479eedb3bf8bc8a0deef7f8e29c
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-05-19 00:38:10 +0200

    Update gauss2d.m
    pass varargins to imgaussfilt

--- code/algorithms/+stack/+process/+filter2/gauss2d.m
+++ code/algorithms/+stack/+process/+filter2/gauss2d.m
@@ -1,11 +1,8 @@
 function imArray = gauss2d(imArray, n, varargin)
-% wrapper for imgaussfilt...
+%gauss2d Wrapper for using imgaussfilt on a 3D array
 
-    % todo: implement varargin
-    if ~isempty(varargin)
-        warning('name value pairs not implemented')
-    end
-
+    % Do we need loop at all? 
+    
     if nargin < 2 || isempty(n)
         n = 0.5;
     end
@@ -13,8 +10,7 @@ function imArray = gauss2d(imArray, n, varargin)
     nFrames = size(imArray, 3);
 
     for i = 1:nFrames
-        imArray(:,:,i) = imgaussfilt(imArray(:,:,i), n);
+        imArray(:,:,i) = imgaussfilt(imArray(:,:,i), n, varargin{:});
     end
-    
     
 end
