02792e4e729e5138a3c6422759383cf8247166cf
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-05-24 18:11:59 +0200

    Create ImageStackIterator.m
    Placeholder/first outline for new class

---
+++ code/datatypes/+nansen/+stack/ImageStackIterator.m
@@ -0,0 +1,20 @@
+classdef ImageStackIterator < handle
+%ImageStackIterator Iterator for frame dimensions of an ImageStack
+%
+%   This class is used by the ImageStackProcessor for managing the
+%   iteration over channels and or planes during processing.
+
+    properties
+        NumIterationsCDimension
+        NumIterationsZDimension
+        
+    end
+
+    methods
+        
+        
+        
+        
+    end
+
+end
