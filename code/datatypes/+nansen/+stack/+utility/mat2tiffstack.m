f56ba9dd11096f828d0193d5d047e09d557e04a9
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-05-29 15:30:39 +0200

    Update mat2tiffstack.m
    Add flag for saving as rgb stack. Necessary if a stack is 3d, but rgb, i.e an rgb stack with 1 frame...
    Also save 4d stack which does not have 3 colors as interleaved 3d stack

--- code/datatypes/+nansen/+stack/+utility/mat2tiffstack.m
+++ code/datatypes/+nansen/+stack/+utility/mat2tiffstack.m
@@ -1,4 +1,4 @@
-function mat2tiffstack( mat, stackPath )
+function mat2tiffstack( mat, stackPath, createRgb)
 %mat2tiffstack writes an uint8 array to a tiff-stack
 %
 %   mat2tiffstack(A, filepath) saves 3D array as a tiff stack in 
@@ -7,6 +7,7 @@ function mat2tiffstack( mat, stackPath )
 % Todo: implement stack order...
 % Note: only works for 3D stack or 4D RGB stack
 
+if nargin < 3; createRgb = false; end
 
 nDim = numel(size(mat));
 className = class(mat);
@@ -23,7 +24,7 @@ switch className
 end
 
 
-if nDim == 2 || nDim == 3
+if (nDim == 2 || nDim == 3) && ~createRgb
     [height, width, nFrames] = size(mat);
 
     tiffFile = Tiff(stackPath, 'w');
@@ -46,24 +47,30 @@ if nDim == 2 || nDim == 3
 
     tiffFile.close();
 
-elseif nDim == 4
+elseif nDim == 4 || createRgb
     
     [height, width, nColors, nFrames] = size(mat);
 
-    assert(nColors == 3)
+    if nColors ~= 3
+        % Write as interleaved 3D stack instead
+        mat = reshape(mat, height, width, []);
+        nansen.stack.utility.mat2tiffstack(mat, stackPath, false)
+    else
+        %assert(nColors == 3)
 
-    tiffFile = Tiff(stackPath,'w');
+        tiffFile = Tiff(stackPath,'w');
 
-    for f = 1:nFrames
-        tiffFile.setTag('ImageLength', height);
-        tiffFile.setTag('ImageWidth', width');
-        tiffFile.setTag('Photometric',Tiff.Photometric.RGB);
-        tiffFile.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
-        tiffFile.setTag('BitsPerSample', bitsPerSample);
-        tiffFile.setTag('SamplesPerPixel', 3);
-        tiffFile.setTag('Compression',Tiff.Compression.None);
-        tiffFile.write(mat(:, :, :, f));
-        tiffFile.writeDirectory();
+        for f = 1:nFrames
+            tiffFile.setTag('ImageLength', height);
+            tiffFile.setTag('ImageWidth', width');
+            tiffFile.setTag('Photometric',Tiff.Photometric.RGB);
+            tiffFile.setTag('PlanarConfiguration',Tiff.PlanarConfiguration.Chunky);
+            tiffFile.setTag('BitsPerSample', bitsPerSample);
+            tiffFile.setTag('SamplesPerPixel', 3);
+            tiffFile.setTag('Compression',Tiff.Compression.None);
+            tiffFile.write(mat(:, :, :, f));
+            tiffFile.writeDirectory();
+        end
     end
 
     tiffFile.close();
