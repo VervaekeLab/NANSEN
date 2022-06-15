d027a5247a608082eb58e07573e66a41ae014bdf
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-06-15 15:05:02 +0200

    Update imviewer.App
    Added dependent props for NumChannels and NumPlanes

--- code/apps/+imviewer/@App/App.m
+++ code/apps/+imviewer/@App/App.m
@@ -100,6 +100,11 @@ properties (SetAccess = private) % Image data properties
     nSamples % Number of samples in imagestack.. (dependent??)
 end
 
+properties (Dependent)
+    NumChannels
+    NumPlanes
+end
+
 properties (Access = public, SetObservable) % Current frame selection
     currentFrameNo (1,1) double = 1 % Current frame number
     currentChannel (1,:) double = 1 % List of channels that are displayed.
@@ -1650,6 +1655,18 @@ methods % Set/Get
         obj.onLinkedAppsSet()
     end
     
+    function isValid = get.Valid(obj)
+        isValid = isvalid(obj) && isvalid(obj.Figure);
+    end
+
+    function numChannels =  get.NumChannels(obj)
+        numChannels = obj.ImageStack.NumChannels;
+    end
+
+    function numPlanes =  get.NumPlanes(obj)
+        numPlanes = obj.ImageStack.NumPlanes;
+    end
+
 end
 
 methods % App update
