bff2d9deb52b35cab401aee854a497af01feebf5
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-09-24 21:14:20 +0200

    Added app utility classes for faster plotting

---
+++ code/graphics/+applify/+utility/LineRecycler.m
@@ -0,0 +1,67 @@
+classdef LineRecycler < applify.utility.abstract.GObjectRecycler
+% A class for line recycling.
+%
+%   This class is useful in apps where the number of plotted lines are
+%   large and where the plotted lines need to be updated fast.
+%
+%   See also applify.utility.abstract.GObjectRecycler
+    
+    properties
+        LineWidth = 0.5;
+        Color = 0.5 * ones(1,3)
+    end
+    
+    methods 
+        
+        function h = getLines(obj, n)
+        %getLines Get a specified number of line objects
+        %
+        %   h = getLines(obj, n) returns a (n x 1) vector of line objects
+
+            h = obj.getGobjects(n);
+        end
+        
+    end
+
+    methods (Access = protected)
+    
+        function h = createNewHandles(obj, n)
+        %createNewHandles Create new line objects    
+            [xInit, yInit] = deal( nan(2, n) );
+            lineProperties = obj.getPropertiesAsNameValuePairs();
+
+            h = line(obj.ParentAxes, xInit, yInit, lineProperties{:});
+        end
+        
+        function h = resetHandleData(~, h)
+        %resetHandleData Reset the x- and y-data of line handles
+            set(h, 'XData', nan, 'YData', nan)
+        end
+
+    end
+
+    methods (Static)
+        function speedtest()
+            performanceTest()
+        end
+    end
+
+end
+
+
+function performanceTest()
+
+    f = figure;
+    ax = axes(f);
+    
+    lineProvider = applify.utility.LineRecycler(ax);
+        
+    fprintf('Creating 2000 lines\n')
+    tic; h = lineProvider.getLines(2000); toc
+
+    fprintf('Recycling 2000 lines\n')
+    tic; lineProvider.recycle(h); toc
+    
+    fprintf('Retrieving 2000 recycled lines\n')
+    tic; h = lineProvider.getLines(2000); toc
+end
---
+++ code/graphics/+applify/+utility/TextRecycler.m
@@ -0,0 +1,68 @@
+classdef TextRecycler < applify.utility.abstract.GObjectRecycler
+% A class for text recycling.
+%
+%   This class is useful in apps where the number of plotted text objects 
+%   are large and where the plotted text need to be updated fast.
+%
+%   See also applify.utility.abstract.GObjectRecycler
+
+    properties
+        FontSize = 10;
+        FontUnits = 'pixels'
+        Color = 0.5 * ones(1,3)
+    end
+    
+    methods 
+        
+        function h = getTextHandles(obj, n)
+        %getTextHandles Get a specified number of text objects
+        %
+        %   h = getTextHandles(obj, n) returns a (n x 1) vector of text objects
+
+            h = obj.getGobjects(n);
+        end
+        
+    end
+
+    methods (Access = protected)
+    
+        function h = createNewHandles(obj, n)
+        %createNewHandles Create new line objects    
+            [xInit, yInit] = deal( zeros(n, 1) );
+            textProperties = obj.getPropertiesAsNameValuePairs();
+
+            h = text(obj.ParentAxes, xInit, yInit, '', textProperties{:});
+        end
+        
+        function h = resetHandleData(~, h)
+        %resetHandleData Reset the x- and y-data of line handles
+            set(h, 'Position', [nan, nan])
+        end
+
+    end
+
+    methods (Static)
+        function speedtest()
+            performanceTest()
+        end
+    end
+
+end
+
+
+function performanceTest()
+
+    f = figure;
+    ax = axes(f);
+    
+    textProvider = applify.utility.TextRecycler(ax);
+        
+    fprintf('Creating 2000 text handles\n')
+    tic; h = textProvider.getTextHandles(2000); toc
+
+    fprintf('Recycling 2000 text handles\n')
+    tic; textProvider.recycle(h); toc
+    
+    fprintf('Retrieving 2000 recycled text handles\n')
+    tic; h = textProvider.getTextHandles(2000); toc
+end
