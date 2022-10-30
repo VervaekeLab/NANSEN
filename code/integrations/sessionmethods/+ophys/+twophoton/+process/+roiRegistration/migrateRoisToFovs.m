b012e9b5ad5a14bec0fbe2e379edf0210e4eb643
Eivind Hennestad <eivinhen@1x-193-157-194-208.uio.no>
2022-10-30 22:46:59 +0100

    Add code for longitudinal roi registration

---
+++ code/integrations/sessionmethods/+ophys/+twophoton/+process/+roiRegistration/migrateRoisToFovs.m
@@ -0,0 +1,60 @@
+function varargout = migrateRoisToFov(sessionObject, varargin)
+%MIGRATEROISTOFOV Summary of this function goes here
+%   Detailed explanation goes here
+
+
+% % % % % % % % % % % % % % CUSTOM CODE BLOCK % % % % % % % % % % % % % % 
+% Create a struct of default parameters (if applicable) and specify one or 
+% more attributes (see nansen.session.SessionMethod.setAttributes) for 
+% details.
+    
+    % Get struct of parameters from local function
+    params = getDefaultParameters();
+    
+    % Create a cell array with attribute keywords
+    ATTRIBUTES = {'batch', 'unqueueable'};   
+
+    
+% % % % % % % % % % % % % DEFAULT CODE BLOCK % % % % % % % % % % % % % % 
+% - - - - - - - - - - Please do not edit this part - - - - - - - - - - - 
+    
+    % Create a struct with "attributes" using a predefined pattern
+    import nansen.session.SessionMethod
+    fcnAttributes = SessionMethod.setAttributes(params, ATTRIBUTES{:});
+    
+    if ~nargin && nargout > 0
+        varargout = {fcnAttributes};   return
+    end
+    
+    % Parse name-value pairs from function input.
+    params = utility.parsenvpairs(params, [], varargin);
+    
+    
+% % % % % % % % % % % % % % CUSTOM CODE BLOCK % % % % % % % % % % % % % % 
+% Implementation of the method : Add your code here:    
+    
+    
+    % Choose reference session
+
+    % Load all fov images
+
+    % Register images for each fov/session to reference fov/session
+    
+    % Load rois for reference session
+
+    % Shift rois for each session based on shifts from image registration
+
+    % Show results
+
+    % Ask to save results
+    
+
+end
+
+
+function S = getDefaultParameters()
+    
+    S = struct();
+    % Add more fields:
+
+end
