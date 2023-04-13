5e39063eff193725e626adae2b315f82d1a24775
ehennestad <ehennestad@gmail.com>
2023-04-13 12:14:48 +0200

    Create placeholder for new location of DataLocationModel

---
+++ code/+nansen/+dataio/@DataLocationModel/DataLocationModel.m
@@ -0,0 +1,7 @@
+classdef DataLocationModel < utility.data.ObjectCatalog
+%DATALOCATIONMODEL Interface for detecting path of data/session folders
+
+% Note: The nansen.config.dloc.DataLocationModel should be moved here
+
+end
+
