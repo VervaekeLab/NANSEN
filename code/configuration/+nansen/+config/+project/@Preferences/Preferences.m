18da3882e59f7d101e5e59f18fa68ffc510ef5db
ehennestad <ehennestad@gmail.com>
2023-09-26 23:27:32 +0200

    Create project preferences

---
+++ code/configuration/+nansen/+config/+project/@Preferences/Preferences.m
@@ -0,0 +1,14 @@
+classdef Preferences < handle
+
+    properties
+        RequiredModule char
+        OptionalModules cell
+    end
+
+    methods (Access = ?nansen.config.project.Project)
+        function obj = Preferences()
+            
+        end
+    end
+
+end
