4b10f64cb2884325682877b1e5a94960c35bd73a
ehennestad <ehennestad@gmail.com>
2023-12-04 13:59:22 +0100

    Move files into code folder

--- apps/Contents.m
+++
@@ -1,8 +0,0 @@
-% APPS Functions for running apps that are included in NANSEN
-%
-% Files
-%   fovmanager   - Open app for registering objects in atlas coordinates 
-%   imviewer     - Open app for viewing and manipulating videos & image stacks
-%   structeditor - Open app to edit the values of a struct
-%   signalviewer - Open an instance of the signalviewer app
-
