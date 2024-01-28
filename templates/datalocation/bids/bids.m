0b7bbb3a23942e633b4f7ef8fc4c148d908ba359
ehennestad <ehennestad@gmail.com>
2024-01-28 19:32:18 +0100

    Added bids data location template

--- templates/datalocation/bids/bids.m
+++ templates/datalocation/bids/bids.m
@@ -1,7 +1,8 @@
-function S = subject_session()
-%SUBJECT_SESSION Template for datalocation organized by subject and session
+function S = bids()
+%BIDS Template for datalocation organized based on bids
 %
-%   S = subject_session() returns a template for a sciscan datalocation 
+%   S = bids() returns a template for a datalocation organized according
+%   to bids
 
     import nansen.config.dloc.DataLocationModel
     
@@ -9,10 +10,10 @@ function S = subject_session()
     S.x_type = 'Nansen Data Location Template Specification';
     S.x_version = '1.0.0';
 
-    S.Name = 'Subject-Session';
-    S.DataType = 'Processed';
+    S.Name = 'BIDS';
+    S.DataType = 'Recorded';
     
-  % % Assign subfolder structure 
+  % % Assign subfolder structure
     [S.SubfolderStructure(1:2)] = ...
         deal(DataLocationModel.getDefaultSubfolderStructure);
     
@@ -21,12 +22,16 @@ function S = subject_session()
 
   % % No metadata specification
     S.MetaDataDef = DataLocationModel.getDefaultMetadataStructure;
-
+    S.MetaDataDef(1).SubfolderLevel = 1;
+    S.MetaDataDef(1).StringDetectInput = '5:end';
+    S.MetaDataDef(2).SubfolderLevel = 2;
+    S.MetaDataDef(2).StringDetectInput = '5:end';
+    
     % Export to module:
     targetPath = fullfile(nansen.common.constant.ModuleRootDirectory, ...
         '+nansen', '+module', '+general', '+core', 'resources', 'datalocations');
 
-    fileName = 'subject_session.json';
+    fileName = 'bids.json';
     savePath = fullfile(targetPath, fileName);
     utility.io.savejson(savePath, S)
 end
