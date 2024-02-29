77277a379803019afeaf9743d8c2ec1a79415173
ehennestad <ehennestad@gmail.com>
2024-02-29 12:21:25 +0100

    Update abspath2funcname.m
    Fix: work for vectors (either cell array or string array)

--- code/general/+utility/+path/abspath2funcname.m
+++ code/general/+utility/+path/abspath2funcname.m
@@ -1,6 +1,14 @@
 function functionName = abspath2funcname(pathStr)
 %abspath2func Get function name for mfile given as pathstr
 
+    if isa(pathStr, 'cell')
+        functionName = cellfun(@(c) utility.path.abspath2funcname(c), pathStr, 'UniformOutput', false);
+        return
+    elseif isa(pathStr, 'string') && numel(pathStr) > 1
+        functionName = arrayfun(@(str) utility.path.abspath2funcname(str), pathStr, 'UniformOutput', false);
+        return
+    end
+
     % Get function name, taking package into account
     [folderPath, functionName, ext] = fileparts(pathStr);
     
