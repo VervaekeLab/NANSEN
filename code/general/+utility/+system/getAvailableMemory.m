4bcf4b5abd70f98abe79f2911b3b08308e5e30a1
ehennestad <ehennestad@gmail.com>
2024-05-18 15:34:11 +0200

    Small changes needed for external PupilTracker

--- code/general/+utility/+system/getAvailableMemory.m
+++ code/general/+utility/+system/getAvailableMemory.m
@@ -1,7 +1,12 @@
 function memoryBytes = getAvailableMemory()
     if ismac
-        [~,txt] = system('sysctl -a | grep hw.memsize | awk ''{print $2}'''); 
-        memory_avail_gb = (eval(txt)/1024^3);
+        try
+            [~,txt] = system('sysctl -a | grep hw.memsize | awk ''{print $2}'''); 
+            memory_avail_gb = (eval(txt)/1024^3);
+        catch
+            [~,txt] = system('sysctl -a | grep hw.memsize_usable | awk ''{print $2}'''); 
+            memory_avail_gb = (eval(txt)/1024^3);
+        end
         memoryBytes = memory_avail_gb*1e9;
     elseif ispc  
         m = memory;
