0330e4314070ba0cd3d0aaeb7d14daa5b1e6e8af
ehennestad <ehennestad@gmail.com>
2024-09-26 12:00:41 +0200

    move install and add requirements

--- install.m
+++ install.m
@@ -6,6 +6,14 @@
 %   2) This script will download dependencies for NANSEN
 %   3) This script will add NANSEN and dependencies to the search path
 
+repoFolder = fileparts(mfilename('fullpath'));
+if isfolder( fullfile(repoFolder, 'code') )
+    addpath(genpath(fullfile(repoFolder, 'code')))
+else
+    error('NANSEN:Setup:CodeFolderNotFound', ...
+          'Could not find folder with code for Nansen')
+end
+
 % Check that userpath is not empty (can happen on linux platforms)
 if isempty(userpath)
     nansen.internal.setup.resolveEmptyUserpath()
@@ -13,8 +21,11 @@ end
 
 % Install required (FEX) dependencies
 fprintf('Installing FileExchange dependencies...\n')
-nansen.internal.setup.installDependencies()
+%nansen.internal.setup.installDependencies()
+
 nansen.internal.setup.installSetupTools()
+toolboxFolder = fileparts(mfilename('fullpath'));
+setuptools.installRequirements(toolboxFolder)
 
 % Add folder to path if it was not added already
 toolboxFolderPath = fileparts(mfilename('fullpath'));
