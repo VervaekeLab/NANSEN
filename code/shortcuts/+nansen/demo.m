06ffebaf6957771a6c679ded5afc4838faa75194
ehennestad <ehennestad@gmail.com>
2024-10-06 11:49:34 -0500

    Rename tutorialWorkflow to loadProject

--- code/shortcuts/+nansen/demo.m
+++ code/shortcuts/+nansen/demo.m
@@ -1,1 +1,5 @@
-nansen.app.tutorial.tutorialWorkflow
+% Install add-ons needed for nansen
+nansen.internal.setup.installAddons()
+
+% Run the tutorial initialization
+nansen.app.tutorial.loadProject()
