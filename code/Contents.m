566ba81752eaf90b1f6e8a91abcd9a94f15127f3
Create new release by ehennestad <>
2024-11-30 11:43:48 +0000

    Final checkins for release v0.9.2

--- code/Contents.m
+++ code/Contents.m
@@ -1,6 +1,6 @@
 % NANSEN - Neuro Analysis Software Ensemble
-% Version 0.9.1 14-06-2024
+% Version 0.9.2 (R2022b+) 30-Nov-2024
 %
-% Copyright (c) 2024 VervaekeLab, Eivind Hennestad
-%
+% Copyright (c) 2024, VervaekeLab
+% -------------------------------
 % Contains code which make up the NANSEN package
