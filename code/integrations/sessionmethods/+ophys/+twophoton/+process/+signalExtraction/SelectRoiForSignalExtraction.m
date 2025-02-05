e322788ba69adfbf8c7056be5189f7e24044d439
ehennestad <ehennestad@gmail.com>
2025-02-05 22:00:42 +0100

    Add method docstring

--- code/integrations/sessionmethods/+ophys/+twophoton/+process/+signalExtraction/SelectRoiForSignalExtraction.m
+++ code/integrations/sessionmethods/+ophys/+twophoton/+process/+signalExtraction/SelectRoiForSignalExtraction.m
@@ -1,7 +1,18 @@
 function varargout = SelectRoiForSignalExtraction(sessionObject, varargin)
-%SELECTROIFORSIGNALEXTRACTION Summary of this function goes here
-%   Detailed explanation goes here
-
+%SELECTROIFORSIGNALEXTRACTION Select which rois to use for signal extraction
+%
+% This method updates the default ROI data variable for a Nansen session. It 
+% prompts the user to select which ROI variable to work with. If a valid 
+% variable name is chosen, the corresponding ROI group is retrieved and
+% saved to the default ROI variable.
+%
+% Note: The default ROI variable is used for signal extraction
+%
+% Parameters:
+%   - deleteRoisOutsideTheBorders (logical) - 
+%     Boolean flag true, determining if ROIs whose pixels lie partially or 
+%     completely outside the field of view (FOV) boundaries are removed. The 
+%     updated ROI group is then saved back to the session.
 
 % % % % % % % % % % % % % % CUSTOM CODE BLOCK % % % % % % % % % % % % % % 
 % Create a struct of default parameters (if applicable) and specify one or 
