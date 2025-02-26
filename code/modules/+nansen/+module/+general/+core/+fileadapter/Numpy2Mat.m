470b33a02347eea8e6f74cf15e48ab46d6f0faf1
ehennestad <ehennestad@gmail.com>
2025-02-26 20:41:54 +0100

    Create Numpy2Mat.m

---
+++ code/modules/+nansen/+module/+general/+core/+fileadapter/Numpy2Mat.m
@@ -0,0 +1,8 @@
+classdef Numpy2Mat < nansen.dataio.fileadapter.numpy
+%NUMPY2MAT File adapter for reading a numpy file
+%
+%   This class makes the Numpy adapter available as part of the core
+%   module
+
+    % See also: nansen.dataio.fileadapter.numpy
+end
