06d24949ae13a265a5d805c39c69c7216c4a185b
ec-eivihe <ec-eivihe@c1-27.fox>
2025-03-17 14:48:02 +0100

    Init user session in nansen.setup if none is active

--- code/+nansen/setup.m
+++ code/+nansen/setup.m
@@ -1,15 +1,29 @@
-% Run setup for nansen
-matlabVersion = version;
-ind = strfind(matlabVersion, '.');
-versionAsNumber = strrep(matlabVersion(1:ind(2)+1), '.', '');
+function setup(userName)
+    arguments
+        userName (1,1) string = ""
+    end
 
-nansen.addpath()
+    userName = char(userName);
 
-if str2double(versionAsNumber) >= 960
-    nansen.app.setup.SetupWizard % Run app coded in appdesigner
-else
-    error('Setup requires MATLAB release 2019a or later')
-    %setup.App
+    try
+        nansen.internal.user.NansenUserSession.instance(userName);
+    catch ME
+        warning(ME.identifier, '%s', ME.message)
+    end
+
+    % Run setup for nansen
+    matlabVersion = version;
+    ind = strfind(matlabVersion, '.');
+    versionAsNumber = strrep(matlabVersion(1:ind(2)+1), '.', '');
+    
+    nansen.addpath()
+    
+    if str2double(versionAsNumber) >= 960
+        nansen.app.setup.SetupWizard % Run app coded in appdesigner
+    else
+        error('Setup requires MATLAB release 2019a or later')
+        %setup.App
+    end
 end
 
 % Todo
