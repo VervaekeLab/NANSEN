1ac2ec1379ac6fd5acd1d4dcc80d27b11c7d2678
ehennestad <ehennestad@gmail.com>
2025-09-01 11:50:53 +0200

    Misc: fix comments

--- code/general/+utility/+path/appendFilenamePostfix.m
+++ code/general/+utility/+path/appendFilenamePostfix.m
@@ -1,7 +1,7 @@
 function newFilename = appendFilenamePostfix(filename, postfix)
 %APPENDFILENAMEPOSTFIX Append a postfix to a filename / filepath
 
-    % Need to strip of the directories if this is a absolute/relative path
+    % Need to strip off the directories if this is a absolute/relative path
     [~, oldFilename] = fileparts( filename );
     newFilename = sprintf('%s_%s', oldFilename, postfix);
     newFilename = strrep(filename, oldFilename, newFilename);
--- code/general/+utility/+struct/sort.m
+++ code/general/+utility/+struct/sort.m
@@ -2,6 +2,6 @@ function sortedS = sort(S, fieldName)
 %sort Sort a struct by values of a field
     
     T = struct2table(S); % convert the struct array to a table
-    sortedT = sortrows(T, fieldName); % sort the table by 'DOB'
-    sortedS = table2struct(sortedT); % change it back to struct array if necessary
+    sortedT = sortrows(T, fieldName); % sort the table by values in given field name
+    sortedS = table2struct(sortedT); % change it back to struct array
 end
