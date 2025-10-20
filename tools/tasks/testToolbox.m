6bb09c1195244cfeaddfa90ddebb0c674096495e
ehennestad <ehennestad@gmail.com>
2025-10-20 23:44:46 +0200

    Ci add coverageignore (#61)
    * Add code coverage ignore support to testToolbox
    
    Introduces a .coverageignore file to exclude specified paths from coverage analysis. Updates testToolbox.m to filter coverage files using ignore patterns, improving coverage reporting accuracy.
    
    * Update testToolbox.m
    
    Fix code issues
    
    * Update coverage ignore path for external files
    
    Fixed the ignored path in .coverageignore from '/code/external' to 'external/fileexchange'
    
    * Customize codecheckToolbox
    
    Ignore external/fileexchange files from code analysis
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tools/tasks/testToolbox.m
+++ tools/tasks/testToolbox.m
@@ -12,10 +12,27 @@ function testToolbox(varargin)
 
     matbox.installRequirements(projectRootDirectory, "AgreeToLicenses", true)
 
+    codecoverageFileList = getCodeCoverageFileList(fullfile(projectRootDirectory, "code")); % local function
+
     matbox.tasks.testToolbox(...
         projectRootDirectory, ...
         "SourceFolderName", "code", ...
+        "CoverageFileList", codecoverageFileList, ...
         "CreateBadge", true, ...
         varargin{:} ...
         )
+end
+
+function fileList = getCodeCoverageFileList(sourceFolder)
+    L = dir( fullfile(sourceFolder, '**', '*.m') );
+
+    fileList = fullfile(string({L.folder}'), string({L.name}'));
+    relativePaths = replace(fileList, sourceFolder + filesep, '');
+
+    coverageIgnoreFile = fullfile(nansentools.projectdir(), 'tests', '.coverageignore');
+    ignorePatterns = string(splitlines( fileread(coverageIgnoreFile) ));
+    ignorePatterns(ignorePatterns=="") = [];
+
+    keep = ~startsWith(relativePaths, ignorePatterns);
+    fileList = fileList(keep);
 end
