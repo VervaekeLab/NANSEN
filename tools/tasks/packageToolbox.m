c4f01887949dcd4ea922302096c36d04e7fdc3f9
ehennestad <ehennestad@gmail.com>
2025-10-31 15:50:01 +0100

    Fix multiple bugs in packageToolbox function
    - Renamed nansen_packageToolbox to packageToolbox
    - Added support for varargin to allow additional arguments.
    - Added a check for '+matbox/installRequirements' before calling nansentools.installMatBox.
    - Updated call to matbox.tasks.packageToolbox to forward varargin.

--- tools/tasks/packageToolbox.m
+++ tools/tasks/packageToolbox.m
@@ -1,13 +1,19 @@
-function [newVersion, mltbxPath] = nansen_packageToolbox(releaseType, versionString)
+function [newVersion, mltbxPath] = packageToolbox(releaseType, versionString, varargin)
     arguments
         releaseType {mustBeTextScalar,mustBeMember(releaseType,["build","major","minor","patch","specific"])} = "build"
         versionString {mustBeTextScalar} = "";
     end
-    
-    nansentools.installMatBox("commit")
+    arguments (Repeating)
+        varargin
+    end
+
+    if exist('+matbox/installRequirements', 'file') ~= 2
+        nansentools.installMatBox("commit")
+    end
 
     projectRootDirectory = nansentools.projectdir();
     [newVersion, mltbxPath] = matbox.tasks.packageToolbox(projectRootDirectory, releaseType, versionString, ...
+        varargin{:}, ...
         "ToolboxShortName", "NANSEN", ...
-        "SourceFolderName", "code")
+        "SourceFolderName", "code");
 end
