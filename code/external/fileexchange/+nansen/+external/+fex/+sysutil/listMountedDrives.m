551a14090911dbd827dbd365fc284e5ad4b89f08
ehennestad <ehennestad@gmail.com>
2025-11-13 10:16:12 +0100

    Update listMountedDrives.m
    Added code for getting drive info on linux
    Removed wmic dependency

--- code/external/fileexchange/+nansen/+external/+fex/+sysutil/listMountedDrives.m
+++ code/external/fileexchange/+nansen/+external/+fex/+sysutil/listMountedDrives.m
@@ -1,8 +1,8 @@
 function infoTable = listMountedDrives()
-%listMountedDrives List physical drives present on system
+%listMountedDrives List mounted drives present on system
 %
-%   infoTable = nansen.external.fex.sysutil.listMountedDrives() returns a table 
-%       which contains the following variables:
+%   infoTable = sysutil.drive.listMountedDrives() returns a table which contains
+%       the following variables:
 %
 %       DeviceID        : The device id (drive letter / disk number)
 %       VolumeName      : Name of drive / volume
@@ -16,29 +16,31 @@ function infoTable = listMountedDrives()
 %   PC  : https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-logicaldisk
 
 % Written by Eivind Hennestad | 2022-11-24
+% Updated by Claude Sonnet 4.5 | 2025-11-13
 
 % Todo:
-% [ ] Implement for linux systems
 % [ ] Add internal, external (how to get this on pc?)
-% [ ] On mac, file system is not correct...
-% [ ] On mac, don't show hidden partitions?
-% [ ] On windows, is the serial number complete?
-% [ ] On mac, add serial number
+% [ ] On mac, file system is not correct...
+% [ ] On mac, don't show hidden partitions?
+% [ ] On mac, add serial number
 % [ ] On mac, parse result when using -plist instead?
-% [ ] On windows, use 'where drivetype=3' i.e 'wmic logicaldisk where drivetype=3 get ...'
+% [ ] On linux, add serial number (use lsblk -o +UUID or blkid)
 
-    if ismac
+    if ismac % Use diskutil
         [~, infoStr] = system('diskutil list physical');
         infoTable = convertListToTableMac(infoStr);
 
-    elseif ispc
-        [~, infoStr] = system(['wmic logicaldisk get DeviceId, ', ...
-            'VolumeName, VolumeSerialNumber, FileSystem, Size, ', ...
-            'DriveType' ] );
+    elseif ispc % Use PowerShell Get-Volume
+        [~, infoStr] = system(['powershell -Command "Get-Volume | ', ...
+            'Where-Object {$_.DriveLetter} | ', ...
+            'Select-Object DriveLetter, FileSystemLabel, FileSystem, ', ...
+            'Size, DriveType | ', ...
+            'ConvertTo-Csv -NoTypeInformation"']);
         infoTable = convertListToTablePc(infoStr);
 
-    elseif isunix
-        error('Not implemented for unix systems')
+    elseif isunix % Use lsblk
+        [~, infoStr] = system('lsblk -o NAME,LABEL,FSTYPE,SIZE,TYPE,MOUNTPOINT -P -b');
+        infoTable = convertListToTableLinux(infoStr);
     end
 
     infoTable = postprocessTable(infoTable);
@@ -59,7 +61,7 @@ function infoTable = convertListToTableMac(infoStr)
     infostrCell = splitStringIntoRows(infoStr);
 
     rowIdxRemove = strncmp(infostrCell, '/dev', 4);
-        
+
     % Keep track of rows belonging to same drive / device
     deviceNumber = cumsum(rowIdxRemove);
     deviceHeaders = infostrCell(rowIdxRemove);
@@ -94,7 +96,7 @@ function infoTable = convertListToTableMac(infoStr)
     expression = '\((.*)\)';
     driveType = regexp(deviceHeaders, expression, 'tokens');
     driveTypeColumnData = arrayfun(@(x) driveType{x}{1}{1}, deviceNumber, 'uni', 0);
-    
+
     colIdx = size(C, 2) + 1;
     C(:, colIdx) = driveTypeColumnData;
 
@@ -112,32 +114,50 @@ end
 
 function infoTable = convertListToTablePc(infoStr)
 
+    % Parse CSV output from PowerShell Get-Volume
     infostrCell = splitStringIntoRows(infoStr);
 
-    % Detect indices where rows should be split
-    colStart = regexp(infostrCell{1}, '(?<=\ )\S{1}', 'start');
-    colStart = [1, colStart];
+    % Remove quotes and split by comma
+    C = cellfun(@(row) strsplit(strrep(row, '"', ''), ','), ...
+        infostrCell, 'UniformOutput', false);
+    C = vertcat(C{:});
 
-    C = splitRowsIntoColumns(infostrCell, colStart);
+    % Rename columns to match expected format
+    C{1,1} = 'DeviceID';
+    C{1,2} = 'VolumeName';
+    C{1,3} = 'FileSystem';
+    C{1,4} = 'Size';
+    C{1,5} = 'DriveType';
 
-    %C{1,6} = 'SerialNumber'; % Shorten name
-    C = strrep(C, 'VolumeSerialNumber', 'SerialNumber');
-    infoTable = cell2table(C(2:end,:), 'VariableNames',C(1,:));
+    % Add colon to drive letters
+    C(2:end, 1) = cellfun(@(x) [x, ':'], C(2:end, 1), 'UniformOutput', false);
+
+    infoTable = cell2table(C(2:end,:), 'VariableNames', C(1,:));
 
     % Compute size and add unit
-    infoTable.Size = str2double( infoTable.Size );
+    infoTable.Size = str2double(infoTable.Size);
 
     power = floor(log10(infoTable.Size)/3)*3;
     infoTable.Size = infoTable.Size ./ 10.^(power);
 
     sizeUnit = categorical(power, [3, 6, 9, 12], {'kB', 'MB', 'GB', 'TB'});
     infoTable = addvars(infoTable, sizeUnit, 'NewVariableNames', 'SizeUnit');
-    
+
+    % Add empty SerialNumber column (Get-Volume doesn't provide this easily)
+    serialNumber = repmat(missing, size(infoTable, 1), 1);
+    infoTable = addvars(infoTable, serialNumber, 'NewVariableNames', 'SerialNumber');
+
+    % Label drive types (handle empty values)
+    for i = 1:height(infoTable)
+        if isempty(infoTable.DriveType{i}) || strcmp(infoTable.DriveType{i}, '')
+            infoTable.DriveType{i} = '3'; % Default to Fixed for empty values
+        end
+    end
     infoTable.DriveType = labelDriveTypePC(infoTable.DriveType);
 end
 
 function infoStrCell = splitStringIntoRows(infoStr)
-    
+
     % Split string into rows
     infoStrCell = textscan( infoStr, '%s', 'delimiter', '\n' );
     infoStrCell = infoStrCell{1};
@@ -147,7 +167,7 @@ function infoStrCell = splitStringIntoRows(infoStr)
 end
 
 function C = splitRowsIntoColumns(infostrCell, splitIdx)
-    
+
     numRows = numel(infostrCell);
     numColumns = numel(splitIdx);
 
@@ -165,23 +185,107 @@ function C = splitRowsIntoColumns(infostrCell, splitIdx)
         colIdx = splitIdx(i) : splitIdx(i+1)-1;
         C(:, i) = cellfun(@(str) str(colIdx), infostrCell, 'uni', 0);
     end
-        
+
     C = strtrim(C); % Remove trailing whitespace from all cells
 end
 
 function driveType = labelDriveTypePC(driveType)
-    
-    %     0	Unknown
-    %     1	No Root Directory
-    %     2	Removable Disk
-    %     3	Local Disk
-    %     4	Network Drive
-    %     5	Compact Disc
-    %     6	RAM Disk
 
-    driveType = categorical(driveType, {'0','1','2','3','4','5','6'}, ...
-        {'Unknown', 'No Root Directory', 'Removable Disk', 'Local Disk', ...
-         'Network Drive', 'Compact Disc', 'RAM Disk'});
+    % Map Get-Volume DriveType values to descriptive names
+    % Get-Volume returns: Unknown=0, Fixed=3, Removable=2, CD-ROM=5, Network=4
+
+    driveType = categorical(driveType, {'0','2','3','4','5'}, ...
+        {'Unknown', 'Removable', 'Fixed', 'Network', 'CD-ROM'});
+end
+
+function infoTable = convertListToTableLinux(infoStr)
+
+    % Parse lsblk output (key="value" format)
+    infostrCell = splitStringIntoRows(infoStr);
+
+    % Keep only mounted drives (has MOUNTPOINT)
+    mountedIdx = contains(infostrCell, 'MOUNTPOINT="/') | contains(infostrCell, 'MOUNTPOINT="/boot');
+    infostrCell = infostrCell(mountedIdx);
+
+    if isempty(infostrCell)
+        % Create empty table with correct structure
+        infoTable = cell2table(cell(0, 7), 'VariableNames', ...
+            {'DeviceID', 'VolumeName', 'SerialNumber', 'FileSystem', 'Size', 'SizeUnit', 'DriveType'});
+        return;
+    end
+
+    numRows = numel(infostrCell);
+    C = cell(numRows, 7);
+
+    for i = 1:numRows
+        row = infostrCell{i};
+
+        % Extract key-value pairs
+        name = extractValue(row, 'NAME');
+        label = extractValue(row, 'LABEL');
+        fstype = extractValue(row, 'FSTYPE');
+        sizeBytes = extractValue(row, 'SIZE');
+        driveType = extractValue(row, 'TYPE');
+        mountpoint = extractValue(row, 'MOUNTPOINT');
+
+        % DeviceID: /dev/name
+        C{i, 1} = ['/dev/', name];
+
+        % VolumeName: use label if available, otherwise mountpoint
+        if isempty(label)
+            C{i, 2} = mountpoint;
+        else
+            C{i, 2} = label;
+        end
+
+        % SerialNumber: not easily available with lsblk
+        C{i, 3} = '';
+
+        % FileSystem
+        C{i, 4} = fstype;
+
+        % Size (in bytes, will convert later)
+        C{i, 5} = sizeBytes;
+
+        % SizeUnit (placeholder, will be computed)
+        C{i, 6} = 'B';
+
+        % DriveType
+        if strcmp(driveType, 'disk')
+            C{i, 7} = 'Fixed';
+        elseif strcmp(driveType, 'part')
+            C{i, 7} = 'Partition';
+        elseif strcmp(driveType, 'loop')
+            C{i, 7} = 'Loop';
+        elseif strcmp(driveType, 'rom')
+            C{i, 7} = 'CD-ROM';
+        else
+            C{i, 7} = driveType;
+        end
+    end
+
+    infoTable = cell2table(C, 'VariableNames', ...
+        {'DeviceID', 'VolumeName', 'SerialNumber', 'FileSystem', 'Size', 'SizeUnit', 'DriveType'});
+
+    % Convert size to numeric and compute appropriate unit
+    infoTable.Size = str2double(infoTable.Size);
+
+    power = floor(log10(infoTable.Size)/3)*3;
+    infoTable.Size = infoTable.Size ./ 10.^(power);
+
+    sizeUnit = categorical(power, [3, 6, 9, 12], {'kB', 'MB', 'GB', 'TB'});
+    infoTable.SizeUnit = sizeUnit;
+end
+
+function value = extractValue(str, key)
+    % Extract value from KEY="VALUE" format
+    pattern = [key, '="([^"]*)"'];
+    tokens = regexp(str, pattern, 'tokens');
+    if ~isempty(tokens)
+        value = tokens{1}{1};
+    else
+        value = '';
+    end
 end
 
 function infoTable = postprocessTable(infoTable)
@@ -207,21 +311,3 @@ function cellArray = removeEmptyCells(cellArray)
     isEmptyCell = cellfun(@isempty, cellArray);
     cellArray( isEmptyCell ) = [];
 end
-
-% % % function filename = filewrite(filename, textString)
-% % %
-% % %     if isempty(filename)
-% % %         filename = [tempname, '.txt'];
-% % %     end
-% % %
-% % %     fid = fopen(filename, 'w');
-% % %     fwrite(fid, textString);
-% % %     fclose(fid);
-% % % end
-% % %
-% % %         [~, infoStr] = system('diskutil list -plist physical');
-% % %
-% % %         filename = [tempname, '.xml'];
-% % %         filename = filewrite(filename, infoStr);
-% % %
-% % %         convertedValue = readstruct(filename);
