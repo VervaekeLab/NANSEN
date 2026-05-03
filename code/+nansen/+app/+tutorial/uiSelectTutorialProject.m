98ff7e933888e30320d16933673b2e68af28d936
ehennestad <ehennestad@gmail.com>
2026-05-03 23:01:54 +0200

    Update tutorial related functions

---
+++ code/+nansen/+app/+tutorial/uiSelectTutorialProject.m
@@ -0,0 +1,13 @@
+function tutorialEnum = uiSelectTutorialProject()
+% Allow user to select an existing project
+    tutorialEnums = enumeration('nansen.app.tutorial.enum.Tutorial');
+    tutorialTitles = [tutorialEnums.Title];
+
+    [selection, ok] = listdlg('ListString', tutorialTitles, 'ListSize', [360, 240]);
+    
+    if ok
+        tutorialEnum = tutorialEnums(selection);
+    else
+        error('User canceled.')
+    end
+end
