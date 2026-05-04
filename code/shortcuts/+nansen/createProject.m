8aa7bd012697ef72180d3167af9df56688d48fd2
ehennestad <ehennestad@gmail.com>
2026-05-04 18:16:59 +0200

    Warning suppression on createProject when no projects exists

--- code/shortcuts/+nansen/createProject.m
+++ code/shortcuts/+nansen/createProject.m
@@ -2,6 +2,11 @@ function createProject(flags)
     arguments (Repeating)
         flags (1,1) string {mustBeMember(flags, ["d", "dependencies", "v", "variables"])}
     end
+
+    % Temporary suppress no projects warning
+    warnState = warning('off', 'Nansen:NoProjectsAvailable');
+    warningCleanup = onCleanup(@() warning(warnState));
+
     % Make sure we don't have a current project selection
     pm = nansen.ProjectManager();
     pm.changeProject('', "Verbose", false);
