7a18610f75fb3f7486c1dc91a9ac2b895cb2f74a
ehennestad <ehennestad@gmail.com>
2026-05-10 21:05:21 +0200

    Update verifyInstallation.m
    Add option to emit warning or error on missing mathworks products

--- code/+nansen/+internal/+setup/verifyInstallation.m
+++ code/+nansen/+internal/+setup/verifyInstallation.m
@@ -1,10 +1,15 @@
-function verifyInstallation()
+function verifyInstallation(options)
 % verifyInstallation - Verify that NANSEN has everything it needs to work
+
+    arguments
+        options.MathworksProductMissingAlertMode = "error"
+    end
 
     nansen.internal.setup.checkWidgetsToolboxVersion();
     
     % Check whether required Mathworks products are installed.
-    nansen.internal.dependencies.checkRequiredMathworksProducts('error')
+    nansen.internal.dependencies.checkRequiredMathworksProducts( ...
+        options.MathworksProductMissingAlertMode)
 
     % Java classpath setup is a recurring pain. Verify that everything is
     % set up correctly before continuing.
